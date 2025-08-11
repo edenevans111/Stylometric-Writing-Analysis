9657125dfb465d7d9cab7f079deb8f8ab39ae3d0
edenevans111 <edenevans111@gmail.com>
2025-08-11 09:39:24 -0700

    added function for counting number of stop words

--- featurizing.py
+++ featurizing.py
@@ -1,4 +1,5 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
 exampleString = "This is an example string to see if I can figure out how to do this"
 
@@ -11,6 +12,22 @@ def word_count(string):
 
 def sentence_count(string):
     return len(sent_tokenize(string))
+
+def unique_word_count(string):
+    pass
+
+def unique_prop(unique, words):
+    return unique/words
+
+def num_stop_words(string):
+    stop_words = set(stopwords.word("english"))
+    all_stops = []
+    for word in string:
+        if word.casefold() in stop_words:
+            all_stops.append(word)
+    return len(all_stops)
+
+
 
 
 def main():
