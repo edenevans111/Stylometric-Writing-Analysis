3a6f3b2b5f7b9fbec352ea1a8f8b6d50f17db2bf
edenevans111 <edenevans111@gmail.com>
2025-08-11 09:52:59 -0700

    Have added unique word count function

--- featurizing.py
+++ featurizing.py
@@ -1,20 +1,18 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
-exampleString = "This is an example string to see if I can figure out how to do this"
-
-word_tokenize(exampleString)
-
-
 def word_count(string):
-    # Obviously this one is for counting the number of words
     return len(word_tokenize(string))
 
 def sentence_count(string):
     return len(sent_tokenize(string))
 
 def unique_word_count(string):
-    pass
+    unique_words = []
+    for word in string:
+        if word not in unique_words:
+            unique_words.append(word)
+    return len(unique_words)
 
 def unique_prop(unique, words):
     return unique/words
@@ -27,6 +25,8 @@ def num_stop_words(string):
             all_stops.append(word)
     return len(all_stops)
 
+def character_count(string):
+    return len(string)
 
 
 
